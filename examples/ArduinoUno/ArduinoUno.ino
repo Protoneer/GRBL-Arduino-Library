3c504c7754bf019eb340369b9572bcd060db6e4c
Bertus Kruger <krugercreations@gmail.com>
2013-03-13 20:48:32 +1300

    Update ArduinoUno.ino

--- examples/ArduinoUno/ArduinoUno.ino
+++ examples/ArduinoUno/ArduinoUno.ino
@@ -1,4 +1,19 @@
-// http://blog.protoneer.co.nz/grbl-arduino-library/
+/*
+  Thanks for supporting Open-Hard/Soft-ware and thanks
+  for all of the contributors to this project.
+  http://blog.protoneer.co.nz/grbl-arduino-library/
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  http://www.gnu.org/licenses/
+*/
 
 #define SETTINGS_ARDUINO_UNO
 #include <grblmain.h>
