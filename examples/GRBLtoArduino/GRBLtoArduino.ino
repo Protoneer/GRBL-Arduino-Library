1e5e8f805aafd54c5f29b46c904446c197467343
Protoneer <krugercreations@gmail.com>
2013-04-27 16:51:48 +1200

    Added new example

---
+++ examples/GRBLtoArduino/GRBLtoArduino.ino
@@ -0,0 +1,27 @@
+/*
+  Thanks for supporting Open-Hard/Soft-ware and thanks
+  for all of the contributors to this project.
+
+  For extra info on GRBL please have a look at my blog : 
+	http://blog.protoneer.co.nz/tag/grbl/
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  http://www.gnu.org/licenses/
+*/
+
+#define SETTINGS_ARDUINO_UNO
+#include <grblmain.h>
+
+void setup(){
+	startGrbl();
+}
+
+void loop(){}
